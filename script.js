318dfed5d236fbf66a80157a0a0ed44e82c65a7b
Mohan Amaravathi <amaravatimohan6@gmail.com>
2026-08-03 13:37:29 +0530

    resolved merge conflict

---
+++ script.js
@@ -0,0 +1,13 @@
+// Welcome message
+console.log("Welcome to MCA Collaboration Project!");
+
+// Function to display a message
+function showMessage() {
+    document.getElementById("message").innerHTML =
+        "Hello! You have successfully clicked the button.";
+}
+
+// Show an alert when the page loads
+window.onload = function () {
+    alert("Welcome to the MCA Collaboration Project!");
+};
